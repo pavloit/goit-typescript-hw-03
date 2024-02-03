35ff38e1d3d41dfdff93c31020979c3cfab4131d
pavloit <dba.devop@gmail.com>
2024-02-03 14:56:13 +0200

    hw3 done

--- src/3.ts
+++ src/3.ts
@@ -1,13 +1,24 @@
 /*
   Ви створюєте гру, де є персонажі з різними ролями.
-  Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
+  Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter 
+  та ISpellCaster.
 
   Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
-  Інтерфейс ISpellCaster повинен включати метод castSpell.
+  Інтерфейс ICharacter повинен включати властивості name і level, і навіть 
+  метод introduce і levelUp. Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
-// реалізація класу Wizard
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -29,7 +40,6 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
 const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,80 @@
-const key = new Key();
+/* 
+У цьому завдання вам належить реалізувати сценарій життя, де людина, ключ і 
+будинок взаємодіють один з одним.
 
+Ключ (Key): Створіть клас Key. У нього має бути одна приватна 
+властивість signature, яка генерується випадково при створенні 
+об'єкта цього класу (наприклад Math.random()). Також цей клас повинен 
+мати метод getSignature, який повертає значення властивості signature.
+
+Людина (Person): Створіть клас Person. Конструктор цього класу приймає об'єкт 
+класу Key і зберігає їх у приватному властивості key. 
+Клас Person повинен мати метод getKey, який повертає збережений ключ.
+
+Дім (House): Створіть абстрактний клас House. 
+Цей клас має дві властивості: door, яка може бути відкрита (true), 
+або закрита (false), і key, яка зберігає об'єкт класу Key. 
+У цьому класі також повинен бути метод comeIn, який додає об'єкт класу 
+Person у масив tenants, якщо door відкрита. Ваш абстрактний клас 
+House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
+
+Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від 
+абстрактного класу House. Реалізуйте метод openDoor у цьому класі. 
+Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, 
+то двері відчиняються.
+
+Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте 
+відтворити сценарій, в якому людина приходить додому.
+*/
+
+class Key {
+    private signature = Math.random().toString();
+    public getSignature(): string {
+      return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+    
+    constructor(protected key: Key) { }
+  
+    public abstract openDoor(key: Key): void;
+  
+    public comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+      } else {
+        console.log("The door is closed");
+      }
+    }
+}
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("Door opened successfully");
+        } else {
+            console.log("The key doesn't match");
+        }
+    }
+}
+    
+    // Usage example:
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
